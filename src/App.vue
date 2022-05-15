853aa8c5bd78ef87fbe8d173c2d21f594f674b2f
Shreyash Payasam <shreyashpayasam@Shreyashs-MacBook-Pro-2.local>
2022-05-15 12:47:45 +0530

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SearchComponent msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchComponent from './components/SearchComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchComponent
   }
 }
 </script>
--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
+    <p class="">
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'SearchComponent',
   props: {
     msg: String
   }
