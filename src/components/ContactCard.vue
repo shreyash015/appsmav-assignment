d44169d8a1200e8ef46f1349f9ea211fb77b033b
Shreyash Payasam <shreyashpayasam@Shreyashs-MacBook-Pro-2.local>
2022-05-22 23:35:21 +0530

    added fake data

--- src/components/ContactCard.vue
+++ src/components/ContactCard.vue
@@ -1,11 +1,20 @@
 <template>
-    <div class=" rounded-md shadow-md">
+    <div class=" rounded-xl shadow-lg">
         <div class="grid grid-cols-4">
             <img class="w-full border " src="https://images.pexels.com/photos/633437/pexels-photo-633437.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="">
             <div class="text-container col-span-3 flex flex-col border text-left pl-4 py-1">
-                <p class="name text-blue-300">Anna Arletti</p>
-                <div class="number">+91 123456788</div>
+                <p class="name text-blue-300">{{`${contact.firstName} ${contact.lastName}`}} </p>
+                <div class="number">{{contact.phoneNumber}}</div>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+    export default {
+       name: 'ContactCard',
+       props:{
+           contact: Object
+       }
+    }
+</script>
--- src/components/ContactsCompnent.vue
+++ src/components/ContactsCompnent.vue
@@ -1,33 +1,69 @@
 <template>
-    <div class="">
-        <h1 class="text-gray-400  uppercase font-bold text-left">My Contact</h1>
-        <SearchComponent/>
-        <div class="contact-card-container mt-8 max-h-full overflow-scroll">
-        <ContactCard @click="this.$router.push('detail')" class="mt-4 cursor-pointer" v-for="index in 10" :key="index"/>
-        </div>
-          <div class="add-contact btn bg-blue-500 rounded-full w-6 mx-auto text-white absolute left-1/2">+</div>
-          <!-- <font-awesome-icon icon="fa-solid fa-circle-plus" /> -->
+  <div class="">
+    <h1 class="text-gray-400 uppercase font-bold text-left">My Contact</h1>
+    <SearchComponent />
+    <div class="contact-card-container mt-8 max-h-full overflow-scroll">
+      <div
+        class="contact-card-container"
+        v-for="contact in sortedContacts()"
+        :key="contact.phoneNumber"
+      >
+        <ContactCard
+          @click="this.$router.push('detail')"
+          class="mt-4 cursor-pointer"
+          :contact="contact"
+          v-if="contacts.length"
+        />
+      </div>
 
+      <div
+        class="add-contact btn bg-blue-500 rounded-full w-6 mx-auto text-white absolute left-1/2"
+      >
+        +
+      </div>
     </div>
+    <!-- <font-awesome-icon icon="fa-solid fa-circle-plus" /> -->
+  </div>
 </template>
 
-
 <script>
-import SearchComponent from '../components/SearchComponent.vue'
-import ContactCard from '../components/ContactCard.vue'
-
+import SearchComponent from "../components/SearchComponent.vue";
+import ContactCard from "../components/ContactCard.vue";
+import contacts from "../generateContacts";
 
 export default {
-  name: 'ContactsCompnent',
-  components:{
+  name: "ContactsCompnent",
+  data() {
+    return {
+      contacts: contacts,
+    };
+  },
+  methods: {
+    sortedContacts() {
+      contacts.sort((a, b) => {
+        let fa = a.firstName.toLowerCase(),
+          fb = b.firstName.toLowerCase();
+
+        if (fa < fb) {
+          return -1;
+        }
+        if (fa > fb) {
+          return 1;
+        }
+        return 0;
+      });
+      return contacts;
+    },
+  },
+  components: {
     SearchComponent,
     ContactCard,
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-  .contact-card-container{
-    max-height: 520px;
-  }
+.contact-card-container {
+  max-height: 520px;
+}
 </style>
